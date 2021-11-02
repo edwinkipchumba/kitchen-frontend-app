e7ab3a81353a571cace9229fd65383b5bec8e172
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-02 13:17:16 +0300

    add routelink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductpageComponent } from './pages/productpage/productpage.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  {path:'product/:id',component:ProductpageComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'registration',component:RegistrationComponent},
+  {path:'login',component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
