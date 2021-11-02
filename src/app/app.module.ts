a6df61313cba0759226524a2b30f2bb53ad2cc7c
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-02 12:42:38 +0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductpageComponent } from './pages/productpage/productpage.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent,
+    ProductpageComponent,
+    NavbarComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
