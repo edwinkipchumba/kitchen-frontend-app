9a66ff7f4d1adfe73a2eaac7eaadf28f720d05b3
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-02 15:57:02 +0300

    add product price display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app.service/Order-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:OrderDetailsService) { }
+  productData:any;
 
   ngOnInit(): void {
+    this.productData = this.service.productDetails;
   }
 
 }
