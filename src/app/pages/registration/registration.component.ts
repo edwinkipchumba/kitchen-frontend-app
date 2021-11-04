c8b821c8c221e39950488f3ece2e73c91a1aa994
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-04 09:30:56 +0300

    validate password

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -12,4 +12,30 @@ export class RegistrationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showSuccessMessage = false;
+
+  // create a new user
+  onSubmit(form: NgForm) {
+   const username = form.value.username;
+   const email = form.value.email;
+   const password = form.value.password;
+   const c_password = form.value.c_password;
+
+   // check if passwords match
+   if (password !== c_password) {
+     alert('Passwords do not match');
+     return;
+   }
+
+   this.service
+     .createUser(username, email, password)
+     .subscribe((response) => {
+       console.log(response);
+       this.showSuccessMessage = true;
+     });
+
+   form.reset();
+
+ }
+
 }
