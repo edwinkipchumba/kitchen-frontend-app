854430eb77c2b440e35ee9f8630c7a8f94a610b2
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-04 09:08:43 +0300

    validate registration

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -12,31 +12,31 @@ export class OrderDetailsService {
   infoDetails = [
     {
       id:1,
-      infoName:"Paneer Grilled Sandwich",
+      infoName:"Open range oven",
       infoDetails:"Pan-fried masala paneer.",
-      infoPrice:200,
+      infoPrice:5000,
       infoImg:"https://images.unsplash.com/photo-1576097449798-7c7f90e1248a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:2,
-      infoName:"Veggie Supreme",
-      infoDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
+      infoName:"Fine Dining Table",
+      infoDetails:"hard,soft wooded and metal type",
       infoPrice:369,
-      infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+      infoImg:"https://images.unsplash.com/photo-1498491751984-14acb85d7d90?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXw4OTMxMTQyfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:3,
-      infoName:"Paneer Burger",
+      infoName:"Kitchen type products",
       infoDetails:"panner",
       infoPrice:149,
-      infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+      infoImg:"https://images.unsplash.com/photo-1631020280895-8449d8e1baaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:4,
-      infoName:"Veg Masala Roll In Naan",
-      infoDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
-      infoPrice:140,
-      infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+      infoName:"Kitchen luxury Dining Table",
+      infoDetails:"",
+      infoPrice:8000,
+      infoImg:"https://media.istockphoto.com/photos/luxury-home-exterior-at-sunset-outdoor-covered-patio-with-kitchen-picture-id1312027291?b=1&k=20&m=1312027291&s=170667a&w=0&h=zsZfv6pmyQbCTkQJ6QKf9ADRGL4CWQ4ZrO9JnrkYgcQ="
     },
     {
       id:5,
