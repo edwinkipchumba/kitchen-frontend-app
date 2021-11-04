868f40e4a3cdacf7f3f4f2f18880146b1c481a21
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-11-04 08:13:21 +0300

    add service

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -4,5 +4,54 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OrderDetailsService {
+
   constructor() { }
+
+    // infodetails 
+  
+    infoDetails = [
+      {
+        id:1,
+        infoName:"Paneer Grilled Sandwich",
+        infoDetails:"Pan-fried masala paneer.",
+        infoPrice:200,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+      },
+      {
+        id:2,
+        infoName:"Veggie Supreme",
+        infoDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
+        infoPrice:369,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+      },
+      {
+        id:3,
+        infoName:"Paneer Burger",
+        infoDetails:"panner",
+        infoPrice:149,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+      },
+      {
+        id:4,
+        infoName:"Veg Masala Roll In Naan",
+        infDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
+        infoPrice:140,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+      },
+      {
+        id:5,
+        infoName:"Indulgence Brownie",
+        infoDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
+        infoPrice:105,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
+      },
+      {
+        id:6,
+        infoName:"Oreo Cheesecake Ice Cream",
+        infoDetails:"Oreo ice cream",
+        infoPrice:219,
+        infoImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
+      }
+    ]
+  
 }
